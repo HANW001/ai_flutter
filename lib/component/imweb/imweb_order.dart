5eb6cbf983ae2c922aa46e9fa56bf7f8763de644
HANW001 <mosadi@nate.com>
2025-03-10 11:35:56 +0900

    add

--- lib/component/imweb/imweb_order.dart
+++ lib/component/imweb/imweb_order.dart
@@ -4,8 +4,6 @@ import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'dart:convert';
 
-import 'package:url_launcher/url_launcher.dart';
-
 class ImwebOrderPage extends StatefulWidget {
   const ImwebOrderPage({super.key});
 
@@ -19,8 +17,6 @@ class _ImwebOrderPageState extends State<ImwebOrderPage> {
     final authState = Provider.of<ImwebAuthState>(context, listen: false);
     final urlState = Provider.of<UrlState>(context, listen: false);
     final accessState = Provider.of<AccessState>(context, listen: false);
-
-    final _urlController = TextEditingController();
 
     Future<void> _oauth() async {
       // var url = Uri.http('10.0.2.2:8000', '/user/login/');
@@ -82,15 +78,12 @@ class _ImwebOrderPageState extends State<ImwebOrderPage> {
 
     Future<void> _access() async {
       // var url = Uri.http('10.0.2.2:8000', '/user/login/');
-      var url = Uri.http('127.0.0.1:8000', '/order/getAccess/');
+      var url = Uri.http('127.0.0.1:8000', '/imweb/getProducts/');
       var response = await http.post(
         url,
         body: {
           'id': authState.id,
-          'code': extractCode(_urlController.text),
-          'clientId': authState.apikey,
-          'clientSecretkey': authState.apikey,
-          // 'servicekey': authState.servicekey,
+          'access_token': urlState.reUrl,
         },
       );
 
@@ -111,24 +104,25 @@ class _ImwebOrderPageState extends State<ImwebOrderPage> {
       }
     }
 
-    Future<void> _order() async {
+    Future<void> _edit() async {
       // var url = Uri.http('10.0.2.2:8000', '/user/login/');
-      var url = Uri.http('127.0.0.1:8000', '/order/getOrder/');
+      var url = Uri.http('127.0.0.1:8000', '/imweb/editProducts/');
       var response = await http.post(
         url,
         body: {
           'id': authState.id,
-          'clientId': authState.apikey,
-          'clientSecretkey': authState.apikey,
-          'servicekey': authState.servicekey,
-          'access_token': accessState.access_token,
+          'access_token': urlState.reUrl,
         },
       );
 
       if (response.statusCode == 200) {
         var jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
-        var data = (jsonResponse);
-        print(data);
+        var data = (jsonResponse['access_token']);
+        Provider.of<AccessState>(context, listen: false).updateUrlData((data));
+
+        // Use listen: false when not updating UI
+        print('Current mall: ${accessState.access_token}');
+
         ScaffoldMessenger.of(context)
             .showSnackBar(const SnackBar(content: Text('데이터 전송에 성공했습니다!')));
       } else {
@@ -148,20 +142,9 @@ class _ImwebOrderPageState extends State<ImwebOrderPage> {
           const SizedBox(height: 16.0),
           reusableElevatedButton('reviews', _reviews),
           const SizedBox(height: 16.0),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'URL',
-            ),
-            controller: _urlController,
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'URL을 입력하세요.';
-              }
-              return null;
-            },
-          ),
-          reusableElevatedButton('access', _access),
-          reusableElevatedButton('order', _order),
+          reusableElevatedButton('product', _access),
+          const SizedBox(height: 16.0),
+          reusableElevatedButton('edit', _edit),
         ],
       ),
     );
